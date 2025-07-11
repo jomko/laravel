bbc80c48e3b29bed3b9249136de1488c064a28dc
jomko <romananas@gmail.com>
2025-07-11 19:32:16 +0300

    Add API V1 ExampleController and route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\V1\ExampleController;
 
 Route::get('/', function () {
     return view('app');
 });
+
+Route::get('/api/v1/example', ExampleController::class);
