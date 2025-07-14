9caa019fb912f2ae62946c196e2bbe54db425095
jomko <romananas@gmail.com>
2025-07-14 03:18:58 +0300

    front

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import DemoPage from './components/DemoPage.vue'
+  import DemoPage from './components/DemoPage.vue'
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen py-2">
       <DemoPage  />
-
   </div>
 </template>
