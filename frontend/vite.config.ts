a45fec7131358adb69493aa0dffe3c3888f2b292
jomko <romananas@gmail.com>
2025-07-16 15:03:55 +0300

    ddev setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true, 
+    port: 5173,
+    allowedHosts: ['morkovka-frontend.ddev.site'], //
+  },
 })
