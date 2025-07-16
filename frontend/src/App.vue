cede3a781953f184b12cc613755f35cc951111e6
jomko <romananas@gmail.com>
2025-07-16 14:31:59 +0300

    add dist folder

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-</script>
+</script> -->
 
 <template>
   <div>
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
 <style scoped>
