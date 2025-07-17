d133c58ff7583025e11982269edc6ed9ad9e0a95
jomko <romananas@gmail.com>
2025-07-18 00:38:36 +0300

    login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
   import DemoPage from './components/DemoPage.vue'
+  import Login from './pages/Login.vue'
+  import { useUserStore } from '@/stores/user'
+
+  const userStore = useUserStore()
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen py-2">
     <img src="/logo.png" alt="">
+    <component :is="userStore.user ? DemoPage : Login" />
     <DemoPage  />
   </div>
 </template>
