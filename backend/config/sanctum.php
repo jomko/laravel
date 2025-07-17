67b1d0d604b98756ef91644af272d4934ba417a1
jomko <romananas@gmail.com>
2025-07-17 23:43:09 +0300

    Install and configure Sanctum

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1')), 
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:5173')),
     'guard' => ['web'],
     'expiration' => null,
     'middleware' => [
