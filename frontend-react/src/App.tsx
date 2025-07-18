a791070831bd5fe0951ebefd1d3b72da88f4c4dd
jomko <romananas@gmail.com>
2025-07-18 18:43:54 +0300

    Add collapsible sidebar with mock menu

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,5 +1,13 @@
 import DemoPage from './DemoPage'
+import Sidebar from './Sidebar'
 
 export default function App() {
-  return <DemoPage />
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1">
+        <DemoPage />
+      </div>
+    </div>
+  )
 }
